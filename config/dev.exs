3aae112ea12f7236163f58b14da3474e899d0a9e
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-02-18 16:04:23 -0500

    some edits

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :tasktracker, TasktrackerWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 5101],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
